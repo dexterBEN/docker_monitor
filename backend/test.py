f15f8a2cfcee05f44fbc2dea44ea5e5dfd6330f4
dexterBEN <bdextereric@yahoo.fr>
2022-05-13 03:03:08 +0200

    test(back): get container by name test

--- backend/test.py
+++ backend/test.py
@@ -14,3 +14,17 @@ def test_get_all_container():
     assert isinstance(containers, list)
     assert type(containers[0]) is dict
     assert containers[0]['Config']['Hostname'] == "dexter-VirtualBox"
+
+
+def test_get_container_by_name():
+
+    #to optimize:
+    urlParams = ['product_service']
+
+    response =  app.test_client().get('/containers/product_service')
+
+    container = json.loads(response.data.decode('utf-8'))
+
+    assert response.status_code == 200
+    assert container != None
+    assert container['Name'] == "/product_service"
