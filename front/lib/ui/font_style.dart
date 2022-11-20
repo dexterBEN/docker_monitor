244b2baab36b4c26f79ffd987af103ede100316f
dexterBEN <bdextereric@yahoo.fr>
2022-11-20 20:22:30 +0100

    chore(front): add const literal

--- front/lib/ui/font_style.dart
+++ front/lib/ui/font_style.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xFF2697FF);
 const bgColor = Color(0xFF212332);
-var secondaryColor = Color(0xFF2A2D3E);
+var secondaryColor = const Color(0xFF2A2D3E);
 
 const defaultPadding = 16.0;
 
 // ignore: prefer_const_constructors
 final txtFontSize24 = TextStyle(fontSize: 24);
 
-final dockerFile_logo =
+const dockerFile_logo =
     "https://res.cloudinary.com/practicaldev/image/fetch/s--rjBiWeZ2--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/6gyu4pn7d0luxk7frb0c.jpg";
--- front/lib/ui/screens/board_content.dart
+++ front/lib/ui/screens/board_content.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_const_constructors
 
-
 import 'package:flutter/material.dart';
 import 'package:front/domain/models/data_categories.dart';
 import 'package:front/domain/models/file.dart';
