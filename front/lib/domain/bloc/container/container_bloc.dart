634960014d7790f0c2f1ed963bd38743a3635bc2
dexterBEN <eric.dexter.benoni@gmail.com>
2024-06-16 11:14:04 -0400

    front: refactoring to one bloc

--- front/lib/domain/bloc/container/container_bloc.dart
+++ front/lib/domain/bloc/container/container_bloc.dart
@@ -25,21 +25,6 @@ class ContainerBloc extends Bloc<ContainerEvent, ContainerState> {
       emit(ListLoaded(containers));
     });
 
-
-    // on<FetchContainerById>((event, emit) async {
-    //   final body = await _containerService.getContainerById(event.containerId);
-
-    //   ContainerData dockerContainer = json.decode(body) as ContainerData;
-    //   print(dockerContainer);
-    //   //emit(ContainerStatusUpdated(newStatus: ))
-    // });
-  }
-}
-
-class ContainerStatusBloc extends Bloc<ContainerEvent, ContainerState> {
-
-  ContainerStatusBloc() : super(InitialeState()){
-
     on<ContainerStart>((event, emit) async {
       emit(ContainerStatusUpdating(containerIdToUpdate: event.containerId));
       var actionStatus =
@@ -76,7 +61,59 @@ class ContainerStatusBloc extends Bloc<ContainerEvent, ContainerState> {
       //print(dockerContainer);
       emit(ContainerFetched(fetchedContainer: containerData));
     });
+
+
+    // on<FetchContainerById>((event, emit) async {
+    //   final body = await _containerService.getContainerById(event.containerId);
+
+    //   ContainerData dockerContainer = json.decode(body) as ContainerData;
+    //   print(dockerContainer);
+    //   //emit(ContainerStatusUpdated(newStatus: ))
+    // });
   }
 }
+
+// class ContainerStatusBloc extends ContainerBloc {
+
+//   ContainerStatusBloc() : super(){
+
+//     on<ContainerStart>((event, emit) async {
+//       emit(ContainerStatusUpdating(containerIdToUpdate: event.containerId));
+//       var actionStatus =
+//           await _containerService.restartContainer(event.containerId);
+
+//       //print(actionStatus);
+
+//       if(actionStatus == 200) {
+//         add(FetchContainerById(containerId: event.containerId));
+//         emit(ContainerStatusUpdated());
+//       }
+//     });
+
+//     on<ContainerStop>((event, emit) async {
+//       emit(ContainerStatusUpdating(containerIdToUpdate: event.containerId));
+//       final actionStatus =  await _containerService.stopContainer(event.containerId);
+
+//       if(actionStatus == 200) {
+//         add(FetchContainerById(containerId: event.containerId));
+//         emit(ContainerStatusUpdated());
+//       }
+//     });
+
+//     on<FetchContainerById>((event, emit) async {
+//       emit(ContainerFetching());
+
+//       final body = await _containerService.getContainerById(event.containerId);
+
+//       final Map<String, dynamic> decodedJson = json.decode(body) as Map<String, dynamic>;
+//       //print(decodedJson.runtimeType);
+//       final ContainerData containerData = ContainerData.fromJson(decodedJson);
+//       //print(containerData);
+
+//       //print(dockerContainer);
+//       emit(ContainerFetched(fetchedContainer: containerData));
+//     });
+//   }
+// }
 
 
