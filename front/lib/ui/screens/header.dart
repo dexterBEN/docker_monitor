ed3daacf091ffe685db8bb0f8a0323a8768ac59f
dexterBEN <eric.dexter.benoni@gmail.com>
2024-07-01 12:30:36 -0400

    fix(front): replace deprecated stuff

--- front/lib/ui/screens/header.dart
+++ front/lib/ui/screens/header.dart
@@ -20,7 +20,7 @@ class Header extends StatelessWidget {
       children: [
         Text(
           "Dashboard",
-          style: Theme.of(context).textTheme.headline6,
+          style: Theme.of(context).textTheme.titleLarge,
         ),
         Spacer(),
         Expanded(
